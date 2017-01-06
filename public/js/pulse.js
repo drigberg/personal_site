d29aa981c0b5e6605c4b79c533edbc663af9ca1a
Daniel Rigberg <daniel.rigberg@gmail.com>
2017-01-06 12:17:12 -0500

    add grid mode

--- public/js/pulse.js
+++ public/js/pulse.js
@@ -2,12 +2,18 @@
 const backgroundColor = "rgba(0, 0, 0, 0)";
 const nodeSize = 8;
 const emptyVector = [0,0];
-const rows = 35;
-const columns = 50;
-const pulseForceConstant = 100;
+const rows = 20;
+const columns = 30;
+const pulseForceConstant = 500;
 const pulseForceExponent = 1.2;
-const attractionToHome = 50;
-const attractionExponent = 1.2;
+const attractionToHome = 0.1;
+const attractionExponent = 1;
+const terminalVelocity = 10;
+const dragStrength = 0.1;
+const recurringPulseFrequency = 1;
+const modes = ["grid", "space"];
+var activeModeIndex = 0;
+var grid;
 
 
 //--------Todo
@@ -75,63 +81,99 @@ function draw() {
 //=========================
 //Classes
 //=========================
-var Node = function(row, column){
-  this.row = row;
-  this.column = column;
-  this.radius = nodeSize;
-  this.homeX = null;
-  this.homeY = null;
-  this.x = null;
-  this.y = null;
-  this.r = 0;
-  this.g = 0;
-  this.b = 0;
-  this.a = 0.5;
-  this.vector = new Vector(0, -1, 0);
-  this.acceleration = 0;
+var Node = function(row, column) {
+    this.row = row;
+    this.column = column;
+    this.radius = nodeSize;
+    this.homeX = null;
+    this.homeY = null;
+    this.x = null;
+    this.y = null;
+    this.r = 0;
+    this.g = 0;
+    this.b = 0;
+    this.a = 0.5;
+    this.vector = new Vector(0, -1, 0);
+    this.acceleration = 0;
+    this.count = 0;
+    this.update = function() {
+        //move node according to motion vector, draw
+        this.accelerate(this.homeX, this.homeY);
+        if (this.vector.magnitude > terminalVelocity) {
+            this.vector.magnitude = terminalVelocity;
+        };
+        this.x += this.vector.x * this.vector.magnitude;
+        this.y += this.vector.y * this.vector.magnitude;
+        ellipse(this.x, this.y, this.radius, this.radius);
+        this.count += 1;
+    };
 
-  this.update = function() {
-      //move node according to motion vector, draw
-      this.x += this.vector.x * this.vector.magnitude;
-      this.y += this.vector.y * this.vector.magnitude;
-      ellipse(this.x, this.y, this.radius, this.radius);
-  };
+    this.accelerate = function(attractionSourceX, attractionSourceY) {
+        if (modes[activeModeIndex] == "grid") {
+            this.vector = this.findAttractionToHome(attractionSourceX, attractionSourceY);
+            this.applyDrag();
+        };
+        if (this.vector.magnitude < 0.1) {
+            this.vector.magnitude = 0;
+        };
+    };
 
-  this.accelerate = function() {
-      distance = findDistance(this.homeX, this.homeY, this.x, this.y);
-      force = attractionToHome / Math.pow(distance, attractionExponent);
+    this.applyDrag = function() {
+        var draggedMagnitude = 0;
+        //   if (this.vector.magnitude < 0 && this.vector.magnitude + dragStrength < 0) {
+        //       draggedMagnitude = this.vector.magnitude + dragStrength;
+        //   } else if (this.vector.magnitude > 0 && this.vector.magnitude - dragStrength > 0) {
+        //       draggedMagnitude = this.vector.magnitude - dragStrength;
+        //   }
 
-      homeUnitVector = findUnitVector(x, y, node.x, node.y);
-      homeNormalVector = convertUnitToNormalVector(homeUnitVector, force);
+        this.vector.magnitude *= (1 - dragStrength);
+            return draggedMagnitude;
+    };
 
-      nodeUnitVector = findUnitVector(0, 0, node.vector.x, node.vector.y);
-      nodeNormalVector = convertUnitToNormalVector(nodeUnitVector, node.vector.magnitude);
+    this.findAttractionToHome = function(attractionSourceX, attractionSourceY ) {
+        distanceFromHome = findDistance(attractionSourceX, attractionSourceY, this.x, this.y);
+        // if (distanceFromHome < 0.1) {
+        //     distanceFromHome = 0.1;
+        // };
 
-      sumVector = findUnitVector(
-          0,
-          0,
-          nodeNormalVector.x + pulseNormalVector.x,
-          nodeNormalVector.y + pulseNormalVector.y
-      );
+        var sumVector = this.vector;
+        var force = attractionToHome * Math.pow(distanceFromHome, attractionExponent);
 
-      sumVector.magnitude = findDistance(
-          0,
-          0,
-          nodeNormalVector.x + pulseNormalVector.x,
-          nodeNormalVector.y + pulseNormalVector.y
-      );
+        var homeAttractionUnitVector = findUnitVector(this.x, this.y, attractionSourceX, attractionSourceY);
+        var homeAttractionNormalVector = convertUnitToNormalVector(homeAttractionUnitVector, force);
 
-      node.vector = sumVector;
-  };
+        var nodeUnitVector = findUnitVector(0, 0, this.vector.x, this.vector.y);
+        var nodeNormalVector = convertUnitToNormalVector(nodeUnitVector, this.vector.magnitude);
+
+        sumVector = findUnitVector(
+            0,
+            0,
+            nodeNormalVector.x + homeAttractionNormalVector.x,
+            nodeNormalVector.y + homeAttractionNormalVector.y
+        );
+
+        sumVector.magnitude = findDistance(
+            0,
+            0,
+            nodeNormalVector.x + homeAttractionNormalVector.x,
+            nodeNormalVector.y + homeAttractionNormalVector.y
+        );
+
+        return sumVector;
+    };
 };
 
-function pulse(x, y) {
+var recurringPulse = function(x, y, strength) {
+
+}
+
+function pulse(x, y, strength) {
     //push all nodes away from pulse location
     for (var col = 0; col < columns; col++){
         for (var row = 0; row < rows; row++){
             var node = grid[col][row];
             var distance = findDistance(x, y, node.x, node.y);
-            var force = pulseForceConstant / Math.pow(distance, pulseForceExponent);
+            var force = strength / Math.pow(distance, pulseForceExponent);
 
             var pulseUnitVector = findUnitVector(x, y, node.x, node.y);
             var pulseNormalVector = convertUnitToNormalVector(pulseUnitVector, force);
@@ -160,7 +202,17 @@ function pulse(x, y) {
 
 function mouseClicked() {
     //set pulse away from mouse click
-    pulse(mouseX, mouseY);
+    pulse(mouseX, mouseY, pulseForceConstant);
+};
+
+function keyPressed() {
+    if (keyCode === 32) {
+        if (activeModeIndex == modes.length - 1) {
+            activeModeIndex = 0;
+        } else {
+            activeModeIndex += 1;
+        };
+    };
 };
 
 
@@ -193,7 +245,11 @@ function findUnitVector(x1, y1, x2, y2) {
     //calculates normal vector between two points (in order), converts to unit vector
     var normalVector = new Vector(x2 - x1, y2 - y1, null);
     var magnitude = sqrt((Math.pow(normalVector.x, 2)) + (Math.pow(normalVector.y, 2)));
-    var unitVector = new Vector(normalVector.x / magnitude, normalVector.y / magnitude, 1);
+    if (magnitude == 0) {
+        var unitVector = new Vector(0, 0, 0);
+    } else {
+        var unitVector = new Vector(normalVector.x / magnitude, normalVector.y / magnitude, 1);
+    };
     return unitVector;
 };
 
@@ -210,7 +266,7 @@ function findDistance(x1, y1, x2, y2) {
     return distance;
 };
 
-function dotProduct(vector1, vector2){
+function dotProduct(vector1, vector2) {
     var dotProduct = vector1.x * vector2.x + vector1.y * vector2.y;
     return dotProduct;
 };
