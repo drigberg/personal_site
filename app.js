d82361c0baaef1db0e982606fabf270ced90924a
Daniel Rigberg <daniel.rigberg@gmail.com>
2017-01-24 19:11:50 -0500

    remove garden project

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ app.get("/", function (req, res){
     res.render("index");
 });
 
-
-app.get("/garden2D", function (req, res){
-    res.render("garden2D");
-});
-
 app.get("/gravitateAndFeed", function (req, res){
     res.render("gravitateAndFeed");
 });
